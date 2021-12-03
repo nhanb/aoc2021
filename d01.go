aa8b98fc1d2876582c188688cb9caeff2ec87815
Bùi Thành Nhân <hi@imnhan.com>
2021-12-03 19:58:03 +0700

    day 1 part 2

--- d01.go
+++ d01.go
@@ -6,9 +6,13 @@ import (
 )
 
 func main() {
+	part2()
+}
+
+func part1() {
 	text := strings.Trim(readFile("d01.input"), "\n")
-	numIncreases := 0
 	lines := strings.Split(text, "\n")
+	numIncreases := 0
 	previousNum, err := strconv.Atoi(lines[0])
 	check(err)
 	for _, line := range lines[1:] {
@@ -21,6 +25,33 @@ func main() {
 			println("---")
 		}
 		previousNum = currentNum
+	}
+	println("Result:", numIncreases)
+}
+
+func part2() {
+	text := strings.Trim(readFile("d01.input"), "\n")
+	lines := strings.Split(text, "\n")
+	numIncreases := 0
+	n1, _ := strconv.Atoi(lines[0])
+	n2, _ := strconv.Atoi(lines[1])
+	n3, _ := strconv.Atoi(lines[2])
+	previousSum := n1 + n2 + n3
+
+	for _, line := range lines[3:] {
+		n1 = n2
+		n2 = n3
+		n3, _ = strconv.Atoi(line)
+		currentSum := n1 + n2 + n3
+		println(n1, n2, n3, previousSum, currentSum)
+		if currentSum > previousSum {
+			numIncreases++
+			println("+++")
+		} else {
+			println("---")
+		}
+		previousSum = currentSum
+
 	}
 	println("Result:", numIncreases)
 }
